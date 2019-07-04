3303121bb2da18807ee6627e33c08d88269b5961
hejiafu <hejiafu@meituan.com>
2019-07-04 10:59:24 +0800

    add log4j2 support

--- src/main/java/com/hzz/controller/HelloController.java
+++ src/main/java/com/hzz/controller/HelloController.java
@@ -1,7 +1,10 @@
 package com.hzz.controller;
 
+import com.hzz.mapper.Tb1Mapper;
 import com.hzz.model.Tb1;
 import com.hzz.service.TestService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,11 +16,18 @@ import javax.annotation.Resource;
 @RestController
 @RequestMapping("/hello")
 public class HelloController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(HelloController.class);
+
     @Resource
     private TestService testService;
 
+    @Resource
+    private Tb1Mapper tb1Mapper;
+
     @RequestMapping("/sayHi")
     public String sayHi(String name) {
+        LOGGER.debug("this is trace log");
+        LOGGER.info("this is info log");
         return "hi, " + name;
     }
 
@@ -26,13 +36,25 @@ public class HelloController {
         return testService.selectById(id);
     }
 
+    @RequestMapping("/selectMany")
+    public String selectMany() {
+        testService.selectById(1L);
+        testService.selectById(1L);
+        return "success";
+    }
+
     @RequestMapping("/trac")
     public String testTransaction(String val1, String val2, String rollback) {
         try {
+            Tb1 t = tb1Mapper.selectByPrimaryKey(1L);
+            System.out.println(t);
+
             testService.add(val1, val2, Boolean.valueOf(rollback));
         } catch (Exception e) {
             e.printStackTrace();
         }
+        Tb1 t = tb1Mapper.selectByPrimaryKey(1L);
+        System.out.println(t);
         return "succ";
     }
 
