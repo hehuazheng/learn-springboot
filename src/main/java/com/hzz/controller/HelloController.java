3be1bb57567421455ac31c060a8c961c262f2bc9
hezz <>
2019-09-03 15:27:30 +0800

    add cookie

--- src/main/java/com/hzz/controller/HelloController.java
+++ src/main/java/com/hzz/controller/HelloController.java
@@ -5,6 +5,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
 /**
  * @author: hezz
  */
@@ -14,8 +17,10 @@ public class HelloController {
     private static final Logger LOGGER = LoggerFactory.getLogger(HelloController.class);
 
     @RequestMapping("/sayHi")
-    public String sayHi(String name) {
+    public String sayHi(String name, HttpServletResponse response) {
         LOGGER.debug("调用HelloController.sayHi参数name = {}", name);
+        Cookie cookie = new Cookie("token", "hezz");
+        response.addCookie(cookie);
         return "hi, " + name;
     }
 }
