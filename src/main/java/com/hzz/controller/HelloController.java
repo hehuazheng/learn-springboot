7b643aa77f6765cbd2e78762cbe469629573ac3f
hezz <>
2019-09-03 16:10:17 +0800

    add

--- src/main/java/com/hzz/controller/HelloController.java
+++ src/main/java/com/hzz/controller/HelloController.java
@@ -2,6 +2,7 @@ package com.hzz.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,11 +17,14 @@ import javax.servlet.http.HttpServletResponse;
 public class HelloController {
     private static final Logger LOGGER = LoggerFactory.getLogger(HelloController.class);
 
+    @Value("${test.username}")
+    private String testUserName;
+
     @RequestMapping("/sayHi")
     public String sayHi(String name, HttpServletResponse response) {
         LOGGER.debug("调用HelloController.sayHi参数name = {}", name);
         Cookie cookie = new Cookie("token", "hezz");
         response.addCookie(cookie);
-        return "hi, " + name;
+        return "hi, " + name + ", " + testUserName;
     }
 }
